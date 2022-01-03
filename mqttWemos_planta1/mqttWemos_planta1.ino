f577cfec0a081b03bbf58eaaed9865db939ea705
Jose Jorge Rodriguez Salgado <josejorgexl@gmail.com>
2022-01-03 18:32:29 +0100

    wemos code

--- mqttWemos_planta1/mqttWemos_planta1.ino
+++ mqttWemos_planta1/mqttWemos_planta1.ino
@@ -1,20 +1,27 @@
 // LIBRARIES 
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+#include "SerialTranfer.h"
 
 //MQTT COMMUNICATION PARAMETERS
 const char* ssid = "WIFI_4C";
 const char* password = "chontaduroexternocleidomastoideo";
 const char* mqtt_server = "192.168.1.64";
+
+// UART COMMUNICATION
+SerialTransfer uart_transfer;
+
 // VARIABLES
 WiFiClient espClient;
 PubSubClient client(espClient);
 unsigned long lastMsg = 0;
 #define MSG_BUFFER_SIZE  (50)
 char msg[MSG_BUFFER_SIZE];
-char* mqtt_in_onoff;
-char* mqtt_in_getparams;
+bool mqtt_in_onoff;
+bool mqtt_in_getparams;
 char* mqtt_in_updtparams;
+char data[20];
+char params[20];
 int value = 0;
 float SP=0.00;
 float PV=0.00;
@@ -32,6 +39,8 @@ void setup() {
   setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
+
+  uart_transfer.begin(Serial);
 }
 
 void loop() {
@@ -39,30 +48,78 @@ void loop() {
   if (!client.connected()) {
     reconnect();
   }
+  
   client.loop();
-  MQTT_OUT();
+
+  /*
+     1. MQTT: RECEIVE on/off from app
+     2. UART: SEND on/off
+     3. UART: RECEIVE data
+     4. UART: RECEIVE parameters
+     5. MQTT: SEND data
+     6. MQTT: RECEIVE parameter request
+     7. MQTT: SEND parameters (if requested)
+     8. MQTT: RECEIVE new parameters
+     9: UART: SEND new parameters
+  */
+
+  if(!mqtt_in_onoff)
+    return;
+    
+  uart_send_on_off();
+  uart_receive_data();
+  uart_receive_params();
+
+  if(mqtt_in_getparams)
+    mqtt_send_params();
+
   
   
-  
-  
-  
- 
 }
-void SERIAL_IN(){}
-void SERIAL_OUT(){}
-void MQTT_IN(){
-  client.subscribe("plant1/on_off");
-  client.subscribe("plant1/get_parameters");
-  client.subscribe("plant1/update_parameters");  
-  }
-void MQTT_OUT(){
-  unsigned long now = 0;
-  now = millis();
-  snprintf (msg, MSG_BUFFER_SIZE, "%f;%f;%f;%f",SP,PV,CP,now);
+
+void uart_send_on_off(){
+  uint16_t sizeData = 0;
+  sizeData =  uart_transfer.txObj(mqtt_in_onoff, sizeData);
+  uart_transfer.sendData(sizeData);
+}
+
+
+void uart_receive_data(){
+  if(!uart_transfer.available())
+    return;
+
+  uint16_t sizeData = 0;
+
+  sizeData = uart_transfer.rxObj(data, sizeData);
+  
+}
+
+void uart_receive_params(){
+  if(!uart_transfer.available())
+    return;
+
+  uint16_t sizeData = 0;
+
+  sizeData = uart_transfer.rxObj(params, sizeData);
+}
+
+void uart_send_params(){
+  uint16_t sizeData = 0;
+  sizeData =  uart_transfer.txObj(params, sizeData);
+  uart_transfer.sendData(sizeData);
+}
+
+void mqtt_send_data(){
   Serial.print("Publish message: ");
-  Serial.println(msg);
-  client.publish("plant1/data", msg);
-  }
+  Serial.println(data);
+  client.publish("plant1/data", data);
+}
+
+void mqtt_send_params(){
+  Serial.print("Publish message: ");
+  Serial.println(params);
+  client.publish("plant1/parameters", params);
+}
 
 
 
@@ -87,46 +144,51 @@ void setup_wifi() {
 
   Serial.println("");
   Serial.println("WiFi connected");
-  
+
+  client.subscribe("plant1/on_off");
+  client.subscribe("plant1/get_parameters");
+  client.subscribe("plant1/update_parameters");
 }
 
 
 
 
 void callback(char* topic, byte* payload, unsigned int length) {
+  
   char inmsg[length+1];
+  
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
+  
   if (topic == "plant1/on_off"){
     for (int i = 0; i < length; i++) {
-    inmsg[i] = (char)payload[i];
+        inmsg[i] = (char)payload[i];
     }
-    inmsg[length]='\0';
-    mqtt_in_onoff=inmsg;
+    inmsg[length] = '\0';
+    mqtt_in_onoff = inmsg == "1";
     
   } else if (topic == "plant1/get_parameters"){
       
       for (int i = 0; i < length; i++) {
-      inmsg[i] = (char)payload[i];
+        inmsg[i] = (char)payload[i];
       }
       inmsg[length]='\0';
-      mqtt_in_getparams=inmsg;
+      mqtt_in_getparams = inmsg == "";
       
-    } else {
+  } else {
         
-        for (int i = 0; i < length; i++) {
+      for (int i = 0; i < length; i++) {
         inmsg[i] = (char)payload[i];
-        }
-        inmsg[length]='\0';
-        mqtt_in_updtparams=inmsg;
-        }
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
+      }
+      inmsg[length]='\0';
+      mqtt_in_updtparams = inmsg;
   }
-  Serial.println();
-
   
+//  for (int i = 0; i < length; i++) {
+//    Serial.print((char)payload[i]);
+//  }
+//  Serial.println();
 }
 
 
